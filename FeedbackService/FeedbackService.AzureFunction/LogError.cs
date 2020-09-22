a63a183c7498b8d909e2800dab60f5ee5128c7e0
Jawwad Mukhtar <jawwad@factor-50.co.uk>
2020-09-22 11:41:38 +0100

    add health check

---
+++ FeedbackService/FeedbackService.AzureFunction/LogError.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace FeedbackService.AzureFunction
+{
+    public static class LogError
+    {
+        public static void Log(ILogger log, Exception exc, Object request)
+        {
+            NewRelic.Api.Agent.NewRelic.NoticeError(exc);
+            log.LogError(exc.ToString());
+        }
+    }
+}
